d6aef64cd28b05c43f8a1daa098a17206d137fbf
imdavidj13-droid <imdavidj13@gmail.com>
2026-08-12 14:06:51 +0100

    Load local settings at startup

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'app/react_app.dart';
+import 'core/settings/react_settings.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
+  await ReactSettings.load();
   runApp(const ReactApp());
 }
