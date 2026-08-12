a88c185c64b4584de3277c24f7026de95e248df5
imdavidj13-droid <imdavidj13@gmail.com>
2026-08-12 13:10:53 +0100

    Lock React to portrait orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'app/react_app.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const ReactApp());
 }
