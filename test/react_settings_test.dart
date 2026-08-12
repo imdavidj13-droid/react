8db2997ad83b2a80ee1aa2f84e492da5f97728c3
imdavidj13-droid <imdavidj13@gmail.com>
2026-08-12 14:34:06 +0100

    Test Pass It player count setting

--- test/react_settings_test.dart
+++ test/react_settings_test.dart
@@ -7,13 +7,15 @@ void main() {
     SharedPreferences.setMockInitialValues(<String, Object>{});
     ReactSettings.soundEnabled = true;
     ReactSettings.visualEffectsEnabled = true;
+    ReactSettings.passItPlayerCount = 3;
   });
 
-  test('settings default to enabled', () async {
+  test('settings default to enabled with three Pass It players', () async {
     await ReactSettings.load();
 
     expect(ReactSettings.soundEnabled, isTrue);
     expect(ReactSettings.visualEffectsEnabled, isTrue);
+    expect(ReactSettings.passItPlayerCount, 3);
   });
 
   test('sound preference persists', () async {
@@ -32,5 +34,22 @@ void main() {
     await ReactSettings.load();
 
     expect(ReactSettings.visualEffectsEnabled, isFalse);
+  });
+
+  test('Pass It player count persists', () async {
+    await ReactSettings.setPassItPlayerCount(4);
+    ReactSettings.passItPlayerCount = 3;
+
+    await ReactSettings.load();
+
+    expect(ReactSettings.passItPlayerCount, 4);
+  });
+
+  test('Pass It player count is clamped to two through four', () async {
+    await ReactSettings.setPassItPlayerCount(99);
+    expect(ReactSettings.passItPlayerCount, 4);
+
+    await ReactSettings.setPassItPlayerCount(0);
+    expect(ReactSettings.passItPlayerCount, 2);
   });
 }
