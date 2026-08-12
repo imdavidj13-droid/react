a1263ad5ada05054816e9d33e0ab5980978de942
imdavidj13-droid <imdavidj13@gmail.com>
2026-08-12 13:09:22 +0100

    Update stale home widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,8 +4,11 @@ import 'package:react/app/react_app.dart';
 void main() {
   testWidgets('shows the React home shell', (tester) async {
     await tester.pumpWidget(const ReactApp());
+    await tester.pump();
 
-    expect(find.text('REACT'), findsOneWidget);
-    expect(find.text('PLAY CLASSIC'), findsOneWidget);
+    expect(find.text('PLAY'), findsOneWidget);
+    expect(find.text('MODES'), findsOneWidget);
+    expect(find.text('DAILY'), findsOneWidget);
+    expect(find.text('SCORES'), findsOneWidget);
   });
 }
