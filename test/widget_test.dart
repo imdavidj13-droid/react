2b47004b8d4483d8b2d6a92d9cf71bc95e179da4
imdavidj13-droid <imdavidj13@gmail.com>
2026-08-12 13:11:12 +0100

    Mock local storage in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:react/app/react_app.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   testWidgets('shows the React home shell', (tester) async {
+    SharedPreferences.setMockInitialValues({});
+
     await tester.pumpWidget(const ReactApp());
-    await tester.pump();
+    await tester.pumpAndSettle();
 
     expect(find.text('PLAY'), findsOneWidget);
     expect(find.text('MODES'), findsOneWidget);
