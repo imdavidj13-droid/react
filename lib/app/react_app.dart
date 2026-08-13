8d0381a8427be4f034d5a2e2f48f035dcb13509b
imdavidj13-droid <imdavidj13@gmail.com>
2026-08-13 03:00:10 +0100

    Expose Daily developer tester in debug builds

--- lib/app/react_app.dart
+++ lib/app/react_app.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+import '../core/theme/react_colors.dart';
 import '../core/theme/react_theme.dart';
+import '../features/daily/presentation/daily_dev_screen.dart';
 import '../features/home/presentation/home_screen.dart';
 
 class ReactApp extends StatelessWidget {
@@ -12,7 +15,43 @@ class ReactApp extends StatelessWidget {
       title: 'React',
       debugShowCheckedModeBanner: false,
       theme: ReactTheme.dark,
-      home: const HomeScreen(),
+      home: kDebugMode ? const _DebugHome() : const HomeScreen(),
+    );
+  }
+}
+
+class _DebugHome extends StatelessWidget {
+  const _DebugHome();
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        const HomeScreen(),
+        Positioned(
+          right: 12,
+          top: MediaQuery.paddingOf(context).top + 10,
+          child: Material(
+            color: Colors.transparent,
+            child: IconButton(
+              tooltip: 'Daily developer tester',
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute<void>(
+                    builder: (_) => const DailyDevScreen(),
+                  ),
+                );
+              },
+              style: IconButton.styleFrom(
+                backgroundColor: const Color(0xFF07111D),
+                foregroundColor: ReactColors.coral,
+                side: const BorderSide(color: ReactColors.coral),
+              ),
+              icon: const Icon(Icons.science_rounded),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
