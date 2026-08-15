63b53d7680f641108cbcf2b8771288703ab26ae8
imdavidj13-droid <imdavidj13@gmail.com>
2026-08-15 06:46:16 +0100

    Cover first-run tutorial app gate

---
+++ test/react_app_first_run_test.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:react/app/react_app.dart';
+import 'package:react/core/settings/react_settings.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  setUp(() {
+    SharedPreferences.setMockInitialValues(<String, Object>{});
+    ReactSettings.howToPlayCompleted = false;
+  });
+
+  testWidgets('fresh install opens How to Play first', (tester) async {
+    ReactSettings.howToPlayCompleted = false;
+
+    await tester.pumpWidget(const ReactApp());
+    await tester.pump();
+
+    expect(find.text('HOW TO PLAY'), findsOneWidget);
+    expect(find.text('9 COMMANDS. ONE SIMPLE RULE.'), findsOneWidget);
+  });
+
+  testWidgets('completed tutorial opens Home instead', (tester) async {
+    ReactSettings.howToPlayCompleted = true;
+
+    await tester.pumpWidget(const ReactApp());
+    await tester.pumpAndSettle();
+
+    expect(find.text('HOW TO PLAY'), findsNothing);
+    expect(find.textContaining('FOLLOW THE COMMAND'), findsOneWidget);
+  });
+}
