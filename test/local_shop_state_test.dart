3097d92ca66a9c1743fc78dcbf037471ab6be050
imdavidj13-droid <imdavidj13@gmail.com>
2026-08-16 06:09:13 +0100

    Test debug visual cosmetic entitlements

--- test/local_shop_state_test.dart
+++ test/local_shop_state_test.dart
@@ -1,26 +1,40 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:react/core/cosmetics/react_cosmetics.dart';
 import 'package:react/features/shop/data/local_shop_state.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  setUp(() {
+  setUp(() async {
     SharedPreferences.setMockInitialValues(<String, Object>{});
+    ReactCosmetics.currentTheme = ReactVisualTheme.core;
+    await LocalShopState.load();
   });
 
   test('core cosmetic is owned and equipped by default', () async {
     expect(LocalShopState.isOwned(LocalShopState.corePackId), isTrue);
-    expect(LocalShopState.isOwned('redline'), isFalse);
     expect(await LocalShopState.equippedPack(), LocalShopState.corePackId);
   });
 
-  test('locked paid pack cannot be equipped locally', () async {
-    await LocalShopState.equip('redline');
-
-    expect(await LocalShopState.equippedPack(), LocalShopState.corePackId);
+  test('implemented visual themes are unlocked in debug builds', () {
+    expect(LocalShopState.debugVisualUnlocksEnabled, isTrue);
+    expect(LocalShopState.isOwned(LocalShopState.redlinePackId), isTrue);
+    expect(LocalShopState.isOwned(LocalShopState.synthwavePackId), isTrue);
+    expect(LocalShopState.isOwned(LocalShopState.monoPackId), isTrue);
+    expect(LocalShopState.isOwned('arcade_sfx'), isFalse);
   });
 
-  test('owned core cosmetic persists when equipped', () async {
-    await LocalShopState.equip(LocalShopState.corePackId);
+  test('debug visual theme can be equipped and persisted', () async {
+    await LocalShopState.equip(LocalShopState.synthwavePackId);
+    expect(await LocalShopState.equippedPack(), LocalShopState.synthwavePackId);
+
+    ReactCosmetics.currentTheme = ReactVisualTheme.core;
+    await LocalShopState.load();
+
+    expect(await LocalShopState.equippedPack(), LocalShopState.synthwavePackId);
+  });
+
+  test('unimplemented paid cosmetic cannot be equipped locally', () async {
+    await LocalShopState.equip('arcade_sfx');
 
     expect(await LocalShopState.equippedPack(), LocalShopState.corePackId);
   });
